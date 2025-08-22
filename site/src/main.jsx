d15e97de459338fe0743f301bc8b10f9323be84a
João Peça <jmpessa@gmail.com>
2025-08-22 02:27:38 +0100

    Home v4

--- site/src/main.jsx
+++ site/src/main.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React, { useEffect } from 'react'
+import { globalCss } from '@stitches/react'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const globalStyles = globalCss({
+  /* Sections are visible by default; reveal animation disabled for now */
+  section: { opacity: 1, translate: '0 0' },
+  // other global styles...
+})
+
+const styles = {
+  header: {
+    // header styles...
+  },
+  // other styles...
+}
+
+export default function App() {
+  useEffect(() => {
+    globalStyles()
+  }, [])
+
+  return (
+    <>
+      <header style={styles.header}>
+        {/* header content */}
+      </header>
+      {/* rest of the app */}
+    </>
+  )
+}
